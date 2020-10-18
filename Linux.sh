14050957c21953838e334d59e57132ea3d7ea32c
Forme-Games <72994284+Forme-Games@users.noreply.github.com>
2020-10-18 09:37:50 +0300

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -1,7 +1,7 @@
-apt install npm
-apt install pip
-npm install vk-io
-pip install vk-api
-apt install nodejs
-npm install colors
+sudo apt-get install npm
+sudo apt-get install python3-pip
+sudo npm install vk-io
+sudo pip install vk-api
+sudo apt install nodejs
+sudo npm install colors
 
