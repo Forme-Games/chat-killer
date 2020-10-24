042d599215624ab6164803bc742bbbb2ca1d563c
Forme-Games <72994284+Forme-Games@users.noreply.github.com>
2020-10-24 12:56:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const TOKEN = "d0875c253a5c58ff093221d99fcb26f6a1840d271a5172c5cdce288e079f9fe051b8bbd39222fb4535d5d"; // Paste token
 const GROUP_ID = 199522266 // Paste id
-const BTN_TEXT = ["vto.pe", "vkbot.ru", "vkbot.ru"];
+const BTN_TEXT = ["vto.pe", "vkbot.ru", "vkbot.ru", "Нажми"];
 const TIME = 10000;
 const CHAT_SPAM = ["@all, привет "];
-const HELLO_TEXT = `Привет) Я снова тут:)
+const HELLO_TEXT = `Привет) Я снова тут :)
 `;
 
 module.exports = {
