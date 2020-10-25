e9a33e6bbf2288e11315e6bd1377d7dccae868a4
Forme-Games <72994284+Forme-Games@users.noreply.github.com>
2020-10-25 16:46:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+clear
+echo "Выберете свой образ:"
+echo "-------------"
+echo "| 1. Linux  |"
+echo "|-----------|"
+echo "| 2. Termux |"
+echo "-------------"
 
+read numb
+if [ $numb = "1" ]
+then
+	clear 
+        sh Linux.sh
+else
+if [$numb = "2" ]
+then
+     clear
+     sh Termux.sh
+
+          fi
+
+     fi
